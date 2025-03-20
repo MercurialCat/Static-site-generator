93c700830258402a84be37fff362ee74a033e9f4
MercurialCat <dbtprivate@gmail.com>
2025-03-20 15:48:59 -0400

    A: First commit of project, Working on finishing my textnode and textnode units tests.

---
+++ src/main.py
@@ -0,0 +1,42 @@
+from textnode import TextNode, TextType
+
+
+def main():
+    node = TextNode(text="This is some anchor text", text_type=TextType.BOLD, url="https://www.boot.dev")
+    print(node)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
